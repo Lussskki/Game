5c2bd5818b9fdd0557f71dd4469376e2a57b368d
Luka Guledani <lukaguledani123@gmail.com>
2024-05-23 15:35:10 +0400

    Refactor player

--- player.py
+++ player.py
@@ -7,22 +7,17 @@ class Player:
     def __init__(self):
         self.x, self.y = player_pos
         self.angle = player_angle
-
     @property
     def pos(self):
         return (self.x, self.y)
-
-    def check_collision(self, x, y):
-        # Check for collision with walls
+    def check_collision(self, x, y):        
         if (int(x // TILE) * TILE, int(y // TILE) * TILE) not in world_map:
             return False
         return True
-
     def movement(self):
         keys = pygame.key.get_pressed()
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
-        
         dx, dy = 0, 0
         speed = player_speed
         if keys[pygame.K_w]:
@@ -37,13 +32,10 @@ class Player:
         if keys[pygame.K_d]:
             dx -= speed * sin_a
             dy += speed * cos_a
-        
-        # Check for collision before updating position
         if not self.check_collision(self.x + dx, self.y):
             self.x += dx
         if not self.check_collision(self.x, self.y + dy):
             self.y += dy
-        
         if keys[pygame.K_LEFT]:
             self.angle -= 0.02
         if keys[pygame.K_RIGHT]:
