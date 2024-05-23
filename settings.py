1bda4dbeae94f42cb586ed97a2b530abadb24bde
Luka Guledani <lukaguledani123@gmail.com>
2024-05-23 15:33:14 +0400

    Create color skyblue

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import math
 
 # Screen settings
-WIDTH = 845
+WIDTH = 840
 HEIGHT = 520
 
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
 
-FPS = 120
+FPS = 60
 TILE = 65
 
 # Ray casting settings
@@ -17,7 +17,7 @@ NUM_RAYS = 120
 MAX_DEPTH = 800
 DELTA_ANGLE = FOV / NUM_RAYS
 DIST = NUM_RAYS / (2 * math.tan(HALF_FOV))
-PROJ_COEFF = DIST * TILE
+PROJ_COEFF = 3 * DIST * TILE
 SCALE = WIDTH // NUM_RAYS
 
 # Player settings
@@ -31,5 +31,6 @@ BLACK = (0, 0, 0)
 RED = (220, 0, 0)
 GREEN = (0, 220, 0)
 BLUE = (0, 0, 220)
-DARKGREY = (110, 110, 110)
+SKYBLUE = (0,186, 255)
+DARKGRAY = (40, 40, 40)
 PURPLE = (120, 0, 120)
